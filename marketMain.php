0214ddf75e7fc9db3e750c20f677fdce4aa1ca7e
MoeinFaghih <momeix.f78@gmail.com>
2025-04-15 22:54:22 +0300

    implemented market add product

--- marketMain.php
+++ marketMain.php
@@ -26,7 +26,26 @@
     <p><?= $user["email"]?></p>
     <p><?= $user["city"]?></p>
     <p><?= $user["district"]?></p>
-
+    <hr>
+    <form action="./market-add-product.php" method="POST">
+        <div>
+            Title: <input type="text" name="title">
+        </div>
+        <div>
+            stock: <input type="text" name="stock">
+        </div>
+        <div>
+            Normal Price: <input type="text" name="n_price">
+        </div>
+        <div>
+            Discounted Price: <input type="text" name="d_price">
+        </div>
+        <div>
+            Expiry Date: <input type="text" name="e_date">
+        </div>
+        <button type="submit">Add Product</button>
+    </form>
+    <hr>
     <?php if(!$products): ?>
         <p>There are no Items yet!</p>
     <?php else: ?>
